5d948c8f8a85230cf55190567a5bb869da2ba51b
MathiasThierry <Mathias@fthierry.dk>
2015-05-04 08:34:35 +0200

    Update

--- app/src/main/java/com/yourdomain/runthedots/Riddleselection.java
+++ app/src/main/java/com/yourdomain/runthedots/Riddleselection.java
@@ -1,23 +1,64 @@
 package com.yourdomain.runthedots;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 
 public class Riddleselection extends ActionBarActivity {
-
+    final Context context = this;
+    private ImageButton Imagebutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_riddleselection);
         // Makes sure that the screen does not go idle and into black screen
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-    }
+        Imagebutton = (ImageButton) findViewById(R.id.Mysterie2Button);
+
+        // add button listener
+        Imagebutton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                final Dialog dialog = new Dialog(context);
+                dialog.setContentView(R.layout.costumize_dialog);
+
+                // set title
+
+                dialog.setTitle("Dette mysterie er låst!");
+
+
+
+
+                // set dialog message
+
+                TextView text = (TextView) dialog.findViewById(R.id.text);
+                text.setText("Gennemfør Mysterie 1 for at åbne op for dette mysterie");
+                Button dialogButton = (Button) dialog.findViewById(R.id.dialogButtonOK);
+                // if button is clicked, close the custom dialog
+                dialogButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+
+                dialog.show();
+            }
+        });
+        }
+
 
 
     @Override
@@ -51,10 +92,4 @@ public class Riddleselection extends ActionBarActivity {
         startActivity(intent);
     }
 
-    // Implements the onClick action that takes you from the riddleselection menu, BACK to the main menu
-    public void FraMysterietilMenu(View view) {
-        Intent intent = new Intent(this, MenuActivity.class);
-        startActivity(intent);
-
-    }
 }
